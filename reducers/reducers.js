f29dee76b8eb21ee9766cb06295d5b5ce3ed40b3
sappidashin <johnny.yeh@sapphireinfo.com.tw>
2016-07-04 17:26:39 +0800

    todo_exercise
    update reducer

--- reducers/reducers.js
+++ reducers/reducers.js
@@ -7,23 +7,42 @@ const initialState = {
 	todos: []
 };
 
-function todoApp(state = initialState, action){
+
+function todos(state = [], action){
 	switch(action.type){
-		case SET_VISIBILITY_FILTER:
-			return Object.assign({}, state, {
-				visibilityFilter: action.filter
-			});
 		case ADD_TODO:
-			return Object.assign({}, state, {
-				todos:[
+			return [
 					...state.todos,
 					{
 						text:action.text,
 						completed: false
 					}
-				]
-			});
+				];
+		case COMPLETE_TODO:
+			return state.todos.map((todo id) => {
+					if(id === action.id)
+						return Object.assign({}, todo, {completed:true});
+
+					return todo;
+				});
 		default:
 			return state;
 	}
+}
+
+function visibilityFilter(state = VisibilityFilters.SHOW_ALL, action){
+	switch(action.type)
+	{
+		case SET_VISIBILITY_FILTER:
+			return action.filter;
+		default:
+			return state;
+	}
+}
+
+function todoApp(state = initialState, action){
+	return {
+		visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+		todos: todos(state.todos, action)
+	};
 }
