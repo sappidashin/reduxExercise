b61cfd31e1bc6cea830fbb83d78107dceeda6b2c
sappidashin <johnny.yeh@sapphireinfo.com.tw>
2016-07-04 17:08:16 +0800

    todo_exercise
    update reducer

---
+++ reducers/reducers.js
@@ -0,0 +1,29 @@
+import {ADD_TODO, COMPLETE_TODO, SET_VISIBILITY_FILTER} from './actions'
+import {VisibilityFilters} from './actions'
+
+
+const initialState = {
+	visibilityFilter: VisibilityFilters.SHOW_ALL,
+	todos: []
+};
+
+function todoApp(state = initialState, action){
+	switch(action.type){
+		case SET_VISIBILITY_FILTER:
+			return Object.assign({}, state, {
+				visibilityFilter: action.filter
+			});
+		case ADD_TODO:
+			return Object.assign({}, state, {
+				todos:[
+					...state.todos,
+					{
+						text:action.text,
+						completed: false
+					}
+				]
+			});
+		default:
+			return state;
+	}
+}
